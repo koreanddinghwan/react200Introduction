b437b2a1f7d25841c28c8f3582ea496366fa1bea
koreanddinghwan <myunghwan0421@gmail.com>
2022-01-03 14:53:27 +0900

    003

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import R003_ImportComponent from "./R003_ImportComponent";
 function App() {
   return (
     <div>
       <h1>Start React</h1>
       <p>HTML적용</p>
+      <R003_ImportComponent></R003_ImportComponent>
     </div>
   );
 }
